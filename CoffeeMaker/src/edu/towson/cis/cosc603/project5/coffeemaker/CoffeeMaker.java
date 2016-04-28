089b958216e22e51be0da788e47d7c8c63b8ae11
oimani1 <oiman1@students.towson.edu>
2016-04-28 01:59:24 -0400

    Auditing Code Task 2 completion.

--- CoffeeMaker/src/edu/towson/cis/cosc603/project5/coffeemaker/CoffeeMaker.java
+++ CoffeeMaker/src/edu/towson/cis/cosc603/project5/coffeemaker/CoffeeMaker.java
@@ -83,7 +83,7 @@ public class CoffeeMaker {
         if(r != null) {
 	        for(int i = 0; i < NUM_RECIPES; i++) {
 	            if(r.equals(recipeArray[i])) {
-	                recipeArray[i] = recipeArray[i]; 
+	                this.recipeArray[i] = recipeArray[i]; 
 	                canDeleteRecipe = true;
 	            }
 	        }
